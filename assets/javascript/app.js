b49f9eca224c22004e9349502a0e767c6316785f
Mark Langhamer <langhamerm@gmail.com>
2018-12-05 15:08:21 -0600

    Updated for hw 9

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+$("#cc-pic").on("click", function(){
+    window.location.href = "https://langhamerm.github.io/unit-4-game/";
+});
+$("#poke-pic").on("click", function(){
+    window.location.href = "https://langhamerm.github.io/TriviaGame/";
+});
+$("#rps-pic").on("click", function(){
+    window.location.href = "https://langhamerm.github.io/RPS-Multiplayer/";
+});
