0b92c91d1cfef9d54c052628c7c70000f976eabb
Mark Langhamer <langhamerm@gmail.com>
2019-02-23 10:14:24 -0600

    Final Touch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,20 @@
-$("#cc-pic").on("click", function(){
-    window.location.href = "https://langhamerm.github.io/unit-4-game/";
-});
-$("#poke-pic").on("click", function(){
-    window.location.href = "https://langhamerm.github.io/TriviaGame/";
-});
-$("#rps-pic").on("click", function(){
-    window.location.href = "https://langhamerm.github.io/RPS-Multiplayer/";
-});
+// DROP DOWN LINKS
+$(".dropbtn").on("click", function () {
+  let dropId = $(this).data("id");
+  $(`.dropdown-content[dropdown-id='${dropId}']`).toggleClass("show");
+})
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
